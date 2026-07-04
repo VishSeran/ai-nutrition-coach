4d41015de77cacbed4ded0b43f1de621441f3171
VishSeran <vishwa.sovis@gmail.com>
2026-07-04 12:47:14 +0530

    image model-get image updated

--- model/image_model.py
+++ model/image_model.py
@@ -1,9 +1,11 @@
 from typing import Optional
-
+from PIL import Image
 from configuration.config import IMAGE_MODEL
 from transformers import AutoImageProcessor, AutoModelForImageTextToText
 import torch
 from configuration.logger import get_logger
+import requests
+import io
 
 logger = get_logger("image-model")
 class ImageModel:
@@ -28,15 +30,43 @@ class ImageModel:
             logger.info(f"{model_name} processor has loaded")
 
         except ValueError as e:
-            print(f"Value error: {e}")
+            logger.error(f"Value error: {e}")
             raise
     
         except Exception as e:
-            print(f"Error in logger initialization: {e}")
+            logger.error(f"Error in Image Model initialization: {e}")
             raise
         
     def image_input(self, 
                     image_path:Optional[str], image_url:Optional[str]):
         
+        image = None
         
+        try:
+            
+            if image_url:
+                response = requests.get(image_url)
+                response.raise_for_status()
+                image = Image.open(io.BytesIO(response.content)).convert("RGB")
+            
+            elif image_path:
+                image = Image.open(image_path).convert("RGB")
+            
+            else:
+                raise ValueError("At least image path or url needed")
+            
+            return image
+                
+            
+        except ValueError as e:
+            logger.error(f"Value error: {e}")
+            raise
+        
+        except requests.RequestException as e:
+            logger.error(f"Error in url fetching:{e}")
+            raise
+    
+        except Exception as e:
+            logger.error(f"Error in image input: {e}")
+            raise
         
