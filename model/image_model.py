0663ad4053e7ddb9d2a9d43bac63953e63527ae3
VishSeran <vishwa.sovis@gmail.com>
2026-07-04 11:10:51 +0530

    image model-init fn updated

---
+++ model/image_model.py
@@ -0,0 +1,17 @@
+from configuration.config import IMAGE_MODEL
+from transformers import AutoImageProcessor, AutoModelForImageTextToText
+import torch
+
+class ImageModel:
+    
+    def __init__(self, model_name=IMAGE_MODEL):
+        
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+        self.model = AutoModelForImageTextToText.from_pretrained(
+            model_name
+        ).to(self.device)
+        
+        self.processor = AutoImageProcessor.from_pretrained(
+            model_name
+        )
