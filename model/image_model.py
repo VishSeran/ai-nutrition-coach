01f03a85e5f0f2b6228c34b62717bf908e1ac707
VishSeran <vishwa.sovis@gmail.com>
2026-07-04 11:14:50 +0530

    image model-get_image fn updated

--- model/image_model.py
+++ model/image_model.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from configuration.config import IMAGE_MODEL
 from transformers import AutoImageProcessor, AutoModelForImageTextToText
 import torch
@@ -5,6 +7,7 @@ import torch
 class ImageModel:
     
     def __init__(self, model_name=IMAGE_MODEL):
+        
         
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         
@@ -15,3 +18,9 @@ class ImageModel:
         self.processor = AutoImageProcessor.from_pretrained(
             model_name
         )
+    
+    def image_input(self, 
+                    image_path:Optional[str], image_url:Optional[str]):
+        
+        
+        
