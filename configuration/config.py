813536bebd1e4ce9282a8fb224d4a1c86b82c362
VishSeran <vishwa.sovis@gmail.com>
2026-07-04 15:48:13 +0530

    faiss process-faisssearch fn updated

--- configuration/config.py
+++ configuration/config.py
@@ -1,5 +1,6 @@
 IMAGE_MODEL = "HuggingFaceTB/SmolVLM2-2.2B-Instruct"
 EMBEDDING_MODEL = "BAAI/bge-base-en"
+DATASET_PATH = "dataset/calories.csv"
 
 system_prompt = """ You are an expert nutritionist. Your task is to analyze the food items displayed in the image and provide the food content and estimated weight of each food item in the following format in a single line:
         1. **Food Item**: Estimate weight in grams."""
