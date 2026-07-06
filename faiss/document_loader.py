7bee2fe417e5894c1b8e6e5041529ec69ca64eab
VishSeran <vishwa.sovis@gmail.com>
2026-07-06 11:42:23 +0530

    document loader fn updated

--- faiss/document_loader.py
+++ faiss/document_loader.py
@@ -1,9 +1,24 @@
 import pandas as pd
 from configuration.config import DATASET_PATH
+from configuration.logger import get_logger
+
+logger = get_logger("document-loader")
 
 class DocumentLoader:
     
     def __init__(self, dataset_name=DATASET_PATH):
         
-        self.df = pd.read_csv(dataset_name)
+        try:
+            if not dataset_name:
+                raise ValueError("dataset name is empty or none")
+            self.df = pd.read_csv(dataset_name)
+            self.documents = []
+            
+        except ValueError as e:
+            logger.error(f"Value error: {e}")
+            raise
+    
+        except Exception as e:
+            logger.error(f"Error in document loader: {e}")
+            raise
         
