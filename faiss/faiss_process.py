dedb8499b39660b2e0f02b18ef232e3e5183bbfa
VishSeran <vishwa.sovis@gmail.com>
2026-07-06 12:30:55 +0530

    faiss process-faisssearch fn updated

--- faiss/faiss_process.py
+++ faiss/faiss_process.py
@@ -2,21 +2,24 @@ import faiss
 from sentence_transformers import SentenceTransformer
 from configuration.config import EMBEDDING_MODEL
 from configuration.logger import get_logger
-from langc
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+import numpy as np
 
 logger = get_logger("faiss-process")
 
 class FaissSearch:
     
-    def __init__(self, embedding_model=EMBEDDING_MODEL):
+    def __init__(self,documents, embedding_model=EMBEDDING_MODEL ):
         
         try:
             if not embedding_model:
                 raise ValueError("embedding model must required")
-        
-            self.embed_model = SentenceTransformer(embedding_model)
-            self.d = self.embed_model.get_embedding_dimension()
-            self.index = faiss.IndexFlatIP(self.d)
+    
+            self.hf_embed_model = HuggingFaceEmbeddings(model_name=embedding_model)
+            dim = len(
+                    self.hf_embed_model.embed_query("hello"))
+            self.index = faiss.IndexFlatIP(dim)
+            logger.info("faiss index and huggingface embedding model have created")
             
         except ValueError as e:
             logger.error(f"Value error: {e}")
@@ -25,4 +28,31 @@ class FaissSearch:
         except Exception as e:
             logger.error(f"Error in Faiss initialization: {e}")
             raise
+        
+    
+    def build_index(self, documents):
+        
+        try:
+            if not documents:
+                raise ValueError("Documents are empty or none")
+            
+            texts = [doc.page_content for doc in documents]
+            embeddings = self.hf_embed_model.embed_documents(texts)
+            
+            embeddings = np.array(
+                embeddings,
+                dtype= np.float32
+            )
+            
+            faiss.normalize_L2(embeddings)
+            self.index.add(embeddings)
+            
+        except ValueError as e:
+            logger.error(f"Value error: {e}")
+            raise
+    
+        except Exception as e:
+            logger.error(f"Error in build faiss index: {e}")
+            raise
+        
     
